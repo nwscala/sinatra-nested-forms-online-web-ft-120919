67d6a7b8d58d8495de3d9080fb8e53cefa2bd0c4
nwscala <nwscala@gmail.com>
2020-02-04 17:30:32 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+    attr_reader :name, :weight, :height
+    @@all = []
+
+    def initialize(pirate_info)
+        @name = pirate_info[:name]
+        @weight = pirate_info[:weight]
+        @height = pirate_info[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_reader :name, :type, :booty
+    @@all = []
+
+    def initialize(ship_info)
+        @name = ship_info[:name]
+        @type = ship_info[:type]
+        @booty = ship_info[:booty]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
